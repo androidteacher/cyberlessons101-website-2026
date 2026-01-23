59efba238f5ba230200da9b4bb966971bdc81e29
CyberLessons101 <joshbeck2006+androidclass@gmail.com>
2026-01-23 10:08:16 -0600

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -5,6 +5,7 @@ import time
 
 # List of commands provided by the user
 commands_text = """
+docker run -d -p 9021:80 -p 9022:22 --network flag-red25-net --cap-add=NET_ADMIN --restart always --name Flag_red25 joshbeck2024/ctf-403-bypass-flag-red25
 docker run -d -p 9000:80 --restart always --name flag-b joshbeck2024/ctf-simple-rev-shell
 docker run -d -p 9001:80 --restart always --name ctf-mime-challenge-flag-n joshbeck2024/ctf-rev-shell-mime-flag-n
 docker run -d -p 9002:80 --restart always --name ctf-base64 joshbeck2024/ctf-base64-rev-shell-flag-k
