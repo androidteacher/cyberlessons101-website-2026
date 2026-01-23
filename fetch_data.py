6ebd7f9f46983746c9da2e84c46540324896df76
CyberLessons101 <joshbeck2006+androidclass@gmail.com>
2026-01-23 11:13:41 -0600

    Add Docker commands to fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -5,6 +5,7 @@ import time
 
 # List of commands provided by the user
 commands_text = """
+docker run -d -p 9023:80 --name Flag-red44 --restart always joshbeck2024/ctf-php-type-juggling-flag-red44
 docker run -d -p 9021:80 -p 9022:22 --network flag-red25-net --cap-add=NET_ADMIN --restart always --name Flag_red25 joshbeck2024/ctf-403-bypass-flag-red25
 docker run -d -p 9000:80 --restart always --name flag-b joshbeck2024/ctf-simple-rev-shell
 docker run -d -p 9001:80 --restart always --name ctf-mime-challenge-flag-n joshbeck2024/ctf-rev-shell-mime-flag-n
