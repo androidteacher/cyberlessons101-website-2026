24b9869f4f970fdbce2fdd761b8a1cd92c52fc5e
CyberLessons101 <joshbeck2006+androidclass@gmail.com>
2026-01-25 10:17:23 -0600

    Update fetch_data.py

--- fetch_data.py
+++ fetch_data.py
@@ -5,6 +5,7 @@ import time
 
 # List of commands provided by the user
 commands_text = """
+docker run -d -p 9032:80 --name flag-red6 --restart always joshbeck2024/ctf-pam_deny-flag-red6
 docker run -d -p 9031:9031 --name flag-red58 --restart always joshbeck2024/ctf-cve-2023-40028-ghost
 docker run -d -p 9030:3000 --name flag-red57 --restart always joshbeck2024/ctf-cve-2025-29927
 docker run -d --privileged --shm-size=2g -p 9029:5000 --name red53 --restart always joshbeck2024/ctf-xss-bot-flag-red53
